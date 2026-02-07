8b907a5885a57583f4d4ccc9943d9e29d5ad4817
RajkiranDev123 <rajtech645@gmail.com>
2026-02-07 11:50:03 +0530

    redux setup

---
+++ frontend/src/store/api.ts
@@ -0,0 +1,25 @@
+// This code creates a central API layer using RTK Query.
+// Instead of:
+// writing fetch / axios everywhere
+// manually handling loading, error, caching, refetching
+// RTK Query does all of that for you.
+
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const Base_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
+
+// createApi → creates an API slice
+
+// fetchBaseQuery → a tiny wrapper around fetch
+// similar to axios
+// handles headers, base URL, credentials, etc.
+export const api = createApi({
+  baseQuery: fetchBaseQuery({
+    baseUrl: Base_URL,
+    credentials: "include",
+  }),
+  tagTypes: ["user"],
+  endpoints: (builder) => ({
+    
+  }),
+});
---
+++ frontend/src/store/store.ts
@@ -0,0 +1,34 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query/react";
+import storage from "redux-persist/lib/storage";
+import {
+  persistReducer,persistStore,FLUSH,REHYDRATE,PAUSE,PURGE,PERSIST,REGISTER,
+} from "redux-persist";
+import userReducer from "./slice/userSlice";
+import { api } from "./api";
+
+const userPersistConfig = {
+  key: "user",
+  storage,
+  whiteList: ["user", "isEmailVerified", "isLoggedIn"],
+};
+
+const persistedUserReducer = persistReducer(userPersistConfig, userReducer);
+
+export const store = configureStore({
+  reducer: {
+    [api.reducerPath]: api.reducer,
+    user: persistedUserReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PURGE, PERSIST, REGISTER],
+      },
+    }).concat(api.middleware),
+});
+setupListeners(store.dispatch);
+
+export const persister = persistStore(store);
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
