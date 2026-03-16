5cffc0fe017dcdaa8556e89f9d01e13d935bdc38
RajkiranDev123 <rajtech645@gmail.com>
2026-03-16 12:30:55 +0530

    google routes

--- backend/routes/authRouter.ts
+++ backend/routes/authRouter.ts
@@ -1,6 +1,9 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import * as authController from "../controllers/authController";
 import { auth } from "../middleware/authMiddleware";
+import passport from "passport";
+import { IUSER } from "../models/User";
+import { generateToken } from "../utils/generateToken";
 
 const router = Router();
 
@@ -11,6 +14,34 @@ router.post("/login", authController.login);
 router.post("/forgot-password", authController.forgotPassword);
 router.post("/reset-password/:token", authController.resetPassword);
 router.get("/logout", authController.logout);
-router.get("/verify-auth",auth, authController.checkUserAuth);
+router.get("/verify-auth", auth, authController.checkUserAuth);
+
+router.get(
+  "/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  }),
+);
+
+router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: `${process.env.FRONTEND_URL}`,
+    session: false,
+  }),
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const user = req.user as IUSER;
+      const accessToken = generateToken(user);
+      res.cookie("access_token", accessToken, {
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000,
+      });
+      res.redirect(`${process.env.FRONTEND_URL}`);
+    } catch (error) {
+      next(error);
+    }
+  },
+);
 
 export default router;
