d6418d4392ca052da57c331d395ee6472bd0caff
RajkiranDev123 <rajtech645@gmail.com>
2026-04-02 09:09:18 +0530

    bug fixed

--- backend/models/Order.ts
+++ backend/models/Order.ts
@@ -32,7 +32,7 @@ const OrderSchema = new Schema<IOrder>(
     user: { type: Schema.Types.ObjectId, ref: "User", required: true },
     items: [OrderItemSchema],
     totalAmount: { type: Number },
-    shippingAddress: [{ type: Schema.Types.ObjectId, ref: "Address" }],
+    shippingAddress: { type: Schema.Types.ObjectId, ref: "Address" },
     paymentStatus: {
       type: String,
       enum: ["pending", "complete", "failed"],
