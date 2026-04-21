3e590bb4c48bf732867db86ecbb7ebf528798177
RajkiranDev123 <rajtech645@gmail.com>
2026-04-21 16:46:25 +0530

    selling product page

--- frontend/src/app/account/selling-products/page.tsx
+++ frontend/src/app/account/selling-products/page.tsx
@@ -1,9 +1,139 @@
-import React from 'react'
+"use client";
+import NoData from "@/app/components/NoData";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import BookLoader from "@/lib/BookLoader";
+import { BookDetails } from "@/lib/types/type";
+import {
+  useDeleteProductByIdMutation,
+  useGetProductBySellerIdQuery,
+} from "@/store/api";
+import { RootState } from "@/store/store";
+import { Package, Trash2 } from "lucide-react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import { useSelector } from "react-redux";
 
 const page = () => {
-  return (
-    <div>page b</div>
-  )
-}
+  const user = useSelector((state: RootState) => state.user.user);
+  const router = useRouter();
+  const { data: products, isLoading } = useGetProductBySellerIdQuery(user?._id);
 
-export default page
+  const [deleteProductById] = useDeleteProductByIdMutation();
+  const [books, setBooks] = useState<BookDetails[]>([]);
+
+  useEffect(() => {
+    if (products?.success) {
+      setBooks(products.data);
+    }
+  }, [products]);
+
+  if (isLoading) {
+    return <BookLoader />;
+  }
+
+  if (books.length === 0) {
+    return (
+      <div className="my-10 max-w-3xl justify-center mx-auto">
+        <NoData
+          imageUrl="/images/no-book.jpg"
+          message="you have not sold any books yet"
+          description="start selling your booksa to reacvh potential buyers,
+        list your first book now and make it available to others"
+          onClick={() => router.push("/book-sell")}
+          ButtonText="sell your first book"
+        />
+      </div>
+    );
+  }
+
+  const handleDeleteProduct = async (productId: string) => {
+    try {
+      await deleteProductById(productId).unwrap();
+      toast.success("book deleted ");
+    } catch (error) {
+      toast.error("failed to delete book");
+    }
+  };
+
+  return (
+    <div className="bg-linear-to-r from-purple-50 to-white py-6">
+      <div className="container mx-auto px-4 max-w-6xl">
+        <div className="mb-10 text-center">
+          <h1 className="text-4xl font-bold text-purple-600 mb-4">
+            your listed books
+          </h1>
+          <p className="text-xl text-gray-600 mb-4">
+            Manage and track your book
+          </p>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
+          {books.map((product: BookDetails) => (
+            <Card
+              key={product._id}
+              className="overflow-hidden shadow-lg hover:shadow-xl
+            
+            transition-shadow duration-300 border-t-4 border-t-purple-500"
+            >
+              <CardHeader className="bg-linear-to-r from-purple-50 to-indigo-50 p-4">
+                <CardTitle className="text-xl text-purple-700 flex items-center">
+                  <Package className="mr-2 h-5 w-5" />
+                  {product.title}
+                </CardTitle>
+                <CardDescription className="">
+                  {product.subject}
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="p-4">
+                <div className="mb-4">
+                  <Image
+                    src={product?.images?.[0]}
+                    alt={product?.title}
+                    width={80}
+                    height={100}
+                    className="object-contain w-60 rounded-lg"
+                  />
+                </div>
+                <div className="space-y-2">
+                  <p className="text-sm text-gray-600">
+                    Category :{product.category}
+                  </p>
+                  <p className="text-sm text-gray-600">
+                    Category :{product.classType}
+                  </p>
+
+                  <div className="flex justify-between items-center">
+                    <Badge
+                      className="bg-purple-100 text-purple-800"
+                      variant={"secondary"}
+                    >
+                      Rs {product.finalPrice}
+                    </Badge>
+                    <span className="text-sm text-gray-500 line-through">{product.price}</span>
+                  </div>
+                </div>
+              </CardContent>
+              <CardFooter className="bg-purple-50 p-4 flex">
+                <Button className="" variant={"destructive"} size={"sm"} onClick={()=>handleDeleteProduct(product?._id)}>
+                  <Trash2 className="h-4 w-4 mr-2"/>Delete
+                </Button>
+              </CardFooter>
+            </Card>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
